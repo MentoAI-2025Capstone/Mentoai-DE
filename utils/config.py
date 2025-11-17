4e95ddb749f99c2c47868e1280191f73ffab3d40
lazzyoung <sprvk0325@naver.com>
2025-11-18 04:36:57 +0900

    feat: 사람인 크롤러 추가, API발급 받은 후 테스트 필요

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    # Saramin
+    SARAMIN_API_KEY = os.getenv("SARAMIN_API_KEY")
+
+    # Linkareer
+    LINKAREER_QUERY_HASH = os.getenv("LINKAREER_QUERY_HASH")
+
+    # DB 정보
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_DB = os.getenv("MYSQL_DB")
